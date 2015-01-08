0d36ccac2d799709a2e55a295db965c5f539efd4
jrgriffiniii <griffinj@lafayette.edu>
2015-01-08 12:11:39 -0500

    Remedying a syntax error within the fedora_commons_solr::params class

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,15 +29,15 @@
 #
 class fedora_commons_solr (
 
-  $fedora_commons_solr::solr_release = params_lookup( 'solr_release' )
-  $fedora_commons_solr::download_url = params_lookup( 'download_url' )
-  $fedora_commons_solr::install_dir_path = params_lookup( 'install_dir_path' )
+  $fedora_commons_solr::solr_release = params_lookup( 'solr_release' ),
+  $fedora_commons_solr::download_url = params_lookup( 'download_url' ),
+  $fedora_commons_solr::install_dir_path = params_lookup( 'install_dir_path' ),
 
-  $fedora_commons_solr::servlet_engine = params_lookup( 'servlet_engine' )
-  $fedora_commons_solr::servlet_webapps_dir_path = params_lookup( 'servlet_webapps_dir_path' )
-  $fedora_commons_solr::servlet_context_dir_path = params_lookup( 'servlet_context_dir_path' )
-  $fedora_commons_solr::servlet_host = params_lookup( 'servlet_host' )
-  $fedora_commons_solr::servlet_port = params_lookup( 'servlet_port' )
+  $fedora_commons_solr::servlet_engine = params_lookup( 'servlet_engine' ),
+  $fedora_commons_solr::servlet_webapps_dir_path = params_lookup( 'servlet_webapps_dir_path' ),
+  $fedora_commons_solr::servlet_context_dir_path = params_lookup( 'servlet_context_dir_path' ),
+  $fedora_commons_solr::servlet_host = params_lookup( 'servlet_host' ),
+  $fedora_commons_solr::servlet_port = params_lookup( 'servlet_port' ),
 
   $fedora_commons_solr::fedora_core_name = params_lookup( 'fedora_core_name' )
 
