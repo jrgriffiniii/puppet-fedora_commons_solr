61222425aaec1e980fc79df59760f9e473f1211c
jrgriffiniii <griffinj@lafayette.edu>
2015-01-08 17:01:58 -0500

    Integrating the configuration for the Apache Cloud configuration; Disabling the timeout for the downloading of Apache Solr

--- manifests/install.pp
+++ manifests/install.pp
@@ -15,6 +15,7 @@ class fedora_commons_solr::install inherits fedora_commons_solr {
   exec { 'download_solr':
 
     command => "/usr/bin/wget ${fedora_commons_solr::download_url} -O /tmp/solr.tgz",
+    timeout => 0,
     unless => '/usr/bin/stat /tmp/solr.tgz'
   }
 
@@ -28,6 +29,13 @@ class fedora_commons_solr::install inherits fedora_commons_solr {
   exec { 'install_solr':
 
     command => "/bin/cp -r /tmp/${fedora_commons_solr::solr_release}/example/solr ${fedora_commons_solr::install_dir_path}",
+    unless => "/usr/bin/stat ${fedora_commons_solr::install_dir_path}",
+    require => Exec['decompress_solr']
+  }
+
+  exec { 'install_solr_libs':
+    
+    command => "/bin/cp -r /tmp/${fedora_commons_solr::solr_release}/dist ${fedora_commons_solr::install_lib_path}",
     unless => "/usr/bin/stat ${fedora_commons_solr::install_dir_path}",
     require => Exec['decompress_solr']
   }
@@ -53,6 +61,17 @@ class fedora_commons_solr::install inherits fedora_commons_solr {
   }
 
   # Copy the /collection1/conf into fedora/conf
+
+  # Deploy Solr with the Cloud configuration
+
+  # Update the config. file for the servlet engine
+  file_line { "solr_insert_servlet_java_opts":
+
+    path => $fedora_commons_solr::servlet_config_path,
+    line => 'JAVA_OPTS="-Djava.library.path=/usr/lib"'
+  }
+
+  # Create the individual nodes for ZooKeeper
   
 
   # Deploy Solr and restart Tomcat
--- manifests/params.pp
+++ manifests/params.pp
@@ -17,10 +17,12 @@ class fedora_commons_solr::params {
   $solr_release = 'solr-4.2.0'
   $download_url = 'http://archive.apache.org/dist/lucene/solr/4.2.0/solr-4.2.0.tgz'
   $install_dir_path = '/usr/share/solr'
+  $install_lib_path = '/usr/share/java/solr'
 
   $servlet_engine = 'tomcat'
   $servlet_webapps_dir_path = '/var/lib/tomcat/webapps'
   $servlet_context_dir_path = '/etc/tomcat/Catalina/localhost'
+  $servlet_config_path = '/etc/tomcat/tomcat.conf'
   $servlet_host = 'localhost'
   $servlet_port = '8080'
 
